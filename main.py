a513a03544139f329c271aafff9131f6860b59b5
omkaraigod-hash <omkaraigod@gmail.com>
2026-06-16 14:41:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,7 @@ sse = SseServerTransport("/messages/")
 
 async def handle_sse(request: Request):
     async with sse.connect_sse(request.scope, request.receive, request._send) as streams:
-        await mcp.run(streams[0], streams[1], mcp.create_initialization_options())
-
+        await mcp._mcp_server.run(streams[0], streams[1], mcp._mcp_server.create_initialization_options())
 app = Starlette(routes=[
     Route("/sse", endpoint=handle_sse),
     Mount("/messages/", app=sse.handle_post_message),
